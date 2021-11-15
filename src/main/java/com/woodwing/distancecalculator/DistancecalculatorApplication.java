f544e7eb562e2bd3fe8ed8ddbf3a3b43337af32f
lucas <lucasmarfe@gmail.com>
2021-11-15 21:29:37 +0100

    TASK-1 <feat> Spring boot maven project setup

---
+++ src/main/java/com/woodwing/distancecalculator/DistancecalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.woodwing.distancecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistancecalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistancecalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woodwing/distancecalculator/DistancecalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woodwing.distancecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistancecalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
